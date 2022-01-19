d32027ac96594ec2f7295c6ecf6e1f657ef1cc0f
Jordan Wildon <88871159+jordanwildon@users.noreply.github.com>
2022-01-19 15:33:00 +0100

    Add files via upload

--- archiver.py
+++ archiver.py
@@ -1,4 +1,4 @@
-#Imports#
+#Imports
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
@@ -6,14 +6,15 @@ from telethon.utils import get_display_name
 import pandas as pd
 import details as ds
 import csv
+import os
 
-#Login details#
+#Login details
 api_id = ds.apiID
 api_hash = ds.apiHash
 phone = ds.number
 client = TelegramClient(phone, api_id, api_hash)
 
-#Check authorisation#
+#Check authorisation
 client.connect()
 if not client.is_user_authorized():
     client.send_code_request(phone)
@@ -38,17 +39,41 @@ for chat in chats:
 
 print('Archiving chats...')
 
-#chatlist.csv to be replaced with your list of channels, must have "To" as a column header#
+#to_archive.csv to be replaced with your list of channels, must have "To" as a column header#
 
 async def main():
     df = pd.read_csv('to_archive.csv')
     df = df.To.unique()
 
     for i in df:
-        print("Working on ", i, "...")
+        print("Working on ", i," This may take a while...")
         l = []
         try:
             async for message in client.iter_messages(i):
+                i_clean = i
+                alphanumeric = ""
+
+                for character in i_clean:
+                    if character.isalnum():
+                        alphanumeric += character
+                directory = './' + alphanumeric
+                try:
+                    os.makedirs(directory)
+                except FileExistsError:
+                    pass
+                media_directory = directory + '/media'
+                try:
+                    os.makedirs(media_directory)
+                except FileExistsError:
+                    pass
+
+                df = pd.DataFrame(l, columns = ['Chat name','message ID','Name','ID','Message text','Message date and time (YYYY/MM/DD, HH:MM)'])
+
+                file = directory + '/'+ alphanumeric + '_archive.csv'
+
+                with open(file, 'r+') as f:
+                    df.to_csv(f, header=False)
+
                 name = get_display_name(message.sender)
                 nameID = message.from_id
                 date = str(message.date.year) + "/" + str(message.date.month) + "/" + str(message.date.day)
@@ -58,20 +83,10 @@ async def main():
                 l.append([i,message.id,name,nameID,message.text,timestamp])
 
                 if message.media:
-                    path = await message.download_media()
-                    print('File saved to', path)
+                    path = await message.download_media(file=media_directory)
+                    #print('File saved to', path)
 
-            df = pd.DataFrame(l, columns = ['Chat name','message ID','Name','ID','Message text','Message date and time (YYYY/MM/DD, HH:MM)'])
-
-            i_clean = i
-            alphanumeric = ""
-
-            for character in i_clean:
-                if character.isalnum():
-                    alphanumeric += character
-
-            df.to_csv(alphanumeric + '_archive.csv')
-            df.to_json(alphanumeric + '_archive.json', orient = 'split', compression = 'infer', index = 'true')
+            df.to_json(directory + alphanumeric + '_archive.json', orient = 'split', compression = 'infer', index = 'true')
 
             print("Scrape completed for", i,", file saved")
 
@@ -80,9 +95,21 @@ async def main():
         except Exception as e:
             print("An exception occurred.", e)
 
-
 with client:
     client.loop.run_until_complete(main())
 
 print('List archived successfully')
-print('Thank you for using Telepathy.')
+
+again = input('Do you want to archive more groups? (y/n)')
+if again == 'y':
+    print('Restarting...')
+    exec(open("archiver.py").read())
+else:
+    pass
+
+launcher = input('Do you want to return to the launcher? (y/n)')
+if launcher == 'y':
+    print('Restarting...')
+    exec(open("telepathy.py").read())
+else:
+    print('Thank you for using Telepathy.')
